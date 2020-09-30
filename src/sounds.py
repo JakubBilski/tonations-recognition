cdd55c267825c916d196b0af85cdba6f223bb9c4
JakubBilski <jakub.michal.bilski@gmail.com>
2020-09-30 18:20:51 +0200

    Make Sound.sounds public, use pathlib everywhere, add args help

--- src/sounds.py
+++ src/sounds.py
@@ -3,7 +3,6 @@ import parselmouth
 import argparse
 import pathlib
 import math
-import os
 
 class Sound:
     def __init__(self, note, timestamp, duration):
@@ -35,45 +34,43 @@ def frequency_to_note(frequency):
 
 class Sounds:
     def __init__(self, file):
-        self._sounds = []
-        snd = parselmouth.Sound(file)
+        self.sounds = []
+        snd = parselmouth.Sound(str(file))
         pitch = snd.to_pitch()
         self._get_sounds_from_pitch(pitch)
 
     def __str__(self):
-        output = ""
-        for sound in self._sounds:
-            output += str(sound)
-        return output
+        return "".join([str(sound) for sound in self.sounds])
 
     def _get_sounds_from_pitch(self, pitch):
-        self._sounds = []
+        self.sounds = []
         frequencies = pitch.selected_array['frequency']
         notes = [frequency_to_note(freq) for freq in frequencies]
         lastNote = None
         lastNoteTimestamp = 0.0
         for note, timestamp in zip(notes, pitch.xs()):
             if lastNote != note:
-                self._sounds.append(Sound(lastNote, lastNoteTimestamp, timestamp-lastNoteTimestamp))
+                self.sounds.append(Sound(lastNote, lastNoteTimestamp, timestamp-lastNoteTimestamp))
                 lastNote = note
                 lastNoteTimestamp = timestamp
-        endTime = pitch.xs()[len(pitch.xs())-1]
-        self._sounds.append(Sound(lastNote, lastNoteTimestamp, endTime-lastNoteTimestamp))
+        endTime = pitch.xs()[-1]
+        self.sounds.append(Sound(lastNote, lastNoteTimestamp, endTime-lastNoteTimestamp))
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Tonations Recognition: ...')
+        description='Convert auio file to notes with time of occurrence')
     parser.add_argument('--input', '-I',
                         required=True,
-                        help='...',
+                        help='Audio file. Formats: [.mp3, .wav]',
                         type=pathlib.Path)
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
-    dirPath = str(parse_args().input)
+    dirPath = parse_args().input
+    
     files = [
-        os.path.join(dirPath, fileName) for fileName in os.listdir(dirPath) if fileName.endswith(".mp3") or fileName.endswith(".wav")]
+        fileName for fileName in dirPath.iterdir() if fileName.suffix in [".mp3", ".wav"]]
     for file in files:
         print(file)
         sounds = Sounds(file)

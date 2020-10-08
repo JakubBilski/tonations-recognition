0e74bdd36953094a319f20a007df749f619a1ee1
Jakub Bilski (OMD) <OMD@kmd.dk>
2020-10-08 15:21:16 +0200

    Rename type to kind, use English chord kinds

--- src/music/tonation.py
+++ src/music/tonation.py
@@ -8,12 +8,12 @@ class Tonation(chord.Chord):
         self.tonic = chord.Chord(note, timestamp, duration, type)
         self.dominant = copy.deepcopy(self.tonic)
         self.dominant.note += 7
-        self.dominant.type = "dur7"
+        self.dominant.kind = "major7"
         self.subdominant = copy.deepcopy(self.tonic)
         self.subdominant.note += 5
 
     def __str__(self):
-        return f"Tonation {self.symbol}-{self.type}\n"\
+        return f"Tonation {self.symbol}-{self.kind}\n"\
             f"tonic: {self.tonic}\n"\
             f"dominant: {self.dominant}\n"\
             f"subdominant: {self.subdominant}"

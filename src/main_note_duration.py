529bd91903afe5016abcb71d1a98697bb97a6497
Jakub Brojacz <kubabr@gmail.com>
2020-10-13 19:27:17 +0200

    Improve sound recognision, add sound generation

--- src/main_note_duration.py
+++ src/main_note_duration.py
@@ -1,7 +1,13 @@
 import argparse
 import pathlib
+import librosa
 
 import sounds_generation
+
+from mingus.containers import Track
+from mingus.midi import midi_file_out
+
+from midi2audio import FluidSynth
 
 
 def parse_args():
@@ -19,5 +25,35 @@ if __name__ == "__main__":
     args = parse_args()
     sounds = sounds_generation.get_sounds_from_file(args.input)
 
-    print("\n".join([str(sound) for sound in sounds]))
-    print("\n".join([f"{sound.duration:.3f}: {sounds_generation.note_to_frequency(sound.note)}" for sound in sounds]))
+    # sounds = [sound for sound in sounds if sound.duration >= 0.1 and sound.note is not None]
+
+    # print("\n".join([str(sound) for sound in sounds]))
+    # print("\n".join([f"{sound.duration:.3f}: {sound.symbol}" for sound in sounds]))
+
+    # d = {}
+    # for s in sounds:
+    #     d[round(100*s.duration)] = d.get(round(100*s.duration), 0)+1
+
+    # ids = sorted(list(d))
+    # for id in ids:
+    #     print(f"{id}: {d[id]}")
+
+    y, sr = librosa.load(args.input)
+    tempo, beats = librosa.beat.beat_track(y=y, sr=sr)
+    first = 60.0/tempo
+    print(first)
+
+    # longest = max(sounds, key=lambda s: s.duration if s.note is not None else 0).duration
+    # print(round(longest*8/first)/8)
+
+    print("\n".join([f"{sound.duration:.3f}: {sound.symbol}\t{round(sound.duration*8/first)/8}" for sound in sounds]))
+
+    t = Track()
+    for s in sounds:
+        if s.note is None:
+            t.add_notes(None, 2)
+        else:
+            t.add_notes(s.symbol, 2)
+    midi_file_out.write_Track("test.mid", t)
+    fs = FluidSynth()
+    fs.midi_to_audio('test.mid', 'test.wav')
--- src/sounds_generation.py
+++ src/sounds_generation.py
@@ -13,10 +13,11 @@ def frequency_to_note(frequency):
         return None
     return round(12*math.log2(frequency/440.0)+45) % 12
 
+
 def note_to_frequency(note):
     if note is None:
         return 0
-    return pow(2,(note-9.0)/12.0)*440.0
+    return pow(2, (note-9.0)/12.0)*440.0
 
 
 def get_sounds_from_file(file):
@@ -25,9 +26,11 @@ def get_sounds_from_file(file):
     frequencies = pitch.selected_array['frequency']
     notes = [frequency_to_note(freq) for freq in frequencies]
     return get_sounds_from_list(pitch.xs(), notes)
-    
+
 
 def get_sounds_from_list(timestamps, notes):
+    # for each timeframe recognise note and
+    # merge same notes together
     sounds = []
     last_note = None
     last_note_timestamp = 0.0
@@ -40,4 +43,13 @@ def get_sounds_from_list(timestamps, notes):
     end_time = timestamps[-1]
     sounds.append(music.Sound(last_note, last_note_timestamp,
                               end_time-last_note_timestamp))
-    return sounds
+
+    # remove badly recognised notes (too short)
+    sounds_corrected = []
+    for s in sounds:
+        if s.duration > 0.1:
+            sounds_corrected.append(s)
+        elif any(sounds_corrected):
+            sounds_corrected[-1].duration += s.duration
+            
+    return sounds_corrected

f0da01188d4701ef8c7b6f9f3eeab35a9fefbf84
Jakub Brojacz <kubabr@gmail.com>
2020-11-28 18:12:07 +0100

    Fix max complexity param

--- src/meter_recognition/fit_to_bar.py
+++ src/meter_recognition/fit_to_bar.py
@@ -28,7 +28,7 @@ def simple_sound_beat_dur(beats, sound):
     return duration_components
 
 
-def update_sounds_with_rhythmic_values_fit_to_bar(tempo, beats, sounds):  # noqa: C901
+def update_sounds_with_rhythmic_values_fit_to_bar(tempo, beats, sounds):
     # lets assume it is 4/4 meter
     # and that min note is thirty two
 

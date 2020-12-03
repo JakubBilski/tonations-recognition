8ecf5a4e52d65cee66cd9351745fa96da113a77b
JakubBilski <jakub.michal.bilski@gmail.com>
2020-12-03 20:55:45 +0100

    Add music classes documentation

--- src/music/tonation.py
+++ src/music/tonation.py
@@ -3,6 +3,18 @@ from . import base_sound
 
 
 class Tonation(base_sound.BaseSound):
+    """A sound of one pitch present in a music piece
+
+    Attributes:
+        kind (str) : "minor" or "major"
+        tonic (Chord) : Set of sounds composing the
+            tonic of this key expressed in a form of Chord
+        subdominant (Chord) : Set of sounds composing the
+            subdominant of this key expressed in a form of Chord
+        dominant (Chord) : Set of sounds composing the
+            dominant of this key expressed in a form of Chord
+        all atributes of the base class (BaseSound)
+    """
     def __init__(self, note=None, symbol=None,
                  kind=None):
         super().__init__(note, symbol)

d76dc872666dd8e2a65e5a4d6f96e722140a0bc4
JakubBilski <jakub.michal.bilski@gmail.com>
2020-12-28 22:49:50 +0100

    Remove duplicate code in test cases initialization

--- src/tests/test_data.py
+++ src/tests/test_data.py
@@ -22,46 +22,19 @@ def get_all_test_models():
 
 
 def get_other_rec_test_models():
-    result = []
     test_data_file = pathlib.Path(
         os.path.realpath(__file__)).parent / "test_data.txt"
-
-    current_testcase = None
-    currect_sounds = None
-    with open(test_data_file, 'r') as f:
-        for line in f:
-            if 'wav' in line:
-                if current_testcase is not None:
-                    current_testcase.sounds = currect_sounds
-                    result.append(current_testcase)
-                current_testcase = TestModel(pathlib.Path(line.strip()))
-                currect_sounds = []
-            elif line.startswith('t'):
-                kind = 'major' if line.split()[1].isupper() else 'minor'
-                current_testcase.key = Key(
-                    symbol=line.split()[1], kind=kind)
-            elif len(line.split()) == 3:
-                spl = line.split()
-                currect_sounds.append(
-                    Sound(symbol=spl[0],
-                          duration=constants.RHYTMIC_VALUE_TO_DURATION[spl[2]])
-                )
-            elif line.startswith('r'):
-                spl = line.split()
-                currect_sounds.append(
-                    Sound(note=None,
-                          duration=constants.RHYTMIC_VALUE_TO_DURATION[spl[1]])
-                )
-    current_testcase.sounds = currect_sounds
-    result.append(current_testcase)
-    return result
+    return _get_test_models(test_data_file)
 
 
 def get_carol_test_models():
-    result = []
     test_data_file = pathlib.Path(
         os.path.realpath(__file__)).parent / "test_carol.txt"
+    return _get_test_models(test_data_file)
 
+
+def _get_test_models(test_data_file):
+    result = []
     current_testcase = None
     currect_sounds = None
     with open(test_data_file, 'r') as f:

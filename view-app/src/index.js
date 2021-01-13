2130cdcf0cfc03e1bf8d5adc07c72d3940f839cd
JakubBilski <jakub.michal.bilski@gmail.com>
2021-01-13 01:25:27 +0100

    Hide inspector

--- view-app/src/index.js
+++ view-app/src/index.js
@@ -47,9 +47,6 @@ const createWindow = () => {
   });
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
   
   mainWindow.setMenuBarVisibility(false);
 };
